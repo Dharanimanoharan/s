02890997ceefdcc61417c47fc7e2e238e2f299e5
Dharanimanoharan <30828412+Dharanimanoharan@users.noreply.github.com>
2017-09-12 16:25:47 +0530

    Create alpha.c

---
+++ alpha.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+ char c;
+ printf("enter any char":);
+ scanf("%d",&c);
+ if(c>='a'&&c<='z')||(c>='A'&&c<='Z')
+ {
+  printf("It is an alphabet number":);
+  }
+  else
+  {
+   printf("It is not a alphabet number":);
+   }
+   }
